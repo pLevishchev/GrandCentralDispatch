0dccdf7a847f82c3198a81ff441c266d92737dbb
Павел Левищев <psihej@gmail.com>
2019-08-29 07:10:32 +0300

    semaphores

---
+++ Semaphores.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+
+let queue = DispatchQueue(label: "test.semaphor", attributes: .concurrent)
+
+let semaphore = DispatchSemaphore(value: 0)
+semaphore.signal()
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    Thread.sleep(forTimeInterval: 4)
+    print("Block 1")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    Thread.sleep(forTimeInterval: 2)
+    print("Block 2")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    print("Block 3")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    print("Block 4")
+    semaphore.signal()
+}
+
